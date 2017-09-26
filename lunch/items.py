1b78fdc9d215e6d8788692fa3bf26947b8fef73d
Niklas Morberg <niklas.morberg@gmail.com>
2017-09-26 18:27:40 +0200

    Define fields for LunchItem

--- lunch/items.py
+++ lunch/items.py
@@ -10,5 +10,5 @@ import scrapy
 
 class LunchItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    weekday = scrapy.Field()
+    courses = scrapy.Field()
