fde4fc2722180967c07d7bbbe77aa2860c51db07
Niklas Morberg <niklas.morberg@gmail.com>
2024-10-07 17:02:46 +0200

    Fixed after web page update

--- lunch/spiders/edison.py
+++ lunch/spiders/edison.py
@@ -3,15 +3,20 @@ import scrapy
 
 
 class EdisonSpider(scrapy.Spider):
-    name = 'edison'
-    allowed_domains = ['restaurangedison.se']
-    start_urls = ['http://restaurangedison.se/lunch']
-    restaurant = 'Ideon Edison'
+    name = "edison"
+    allowed_domains = ["restaurangedison.se"]
+    start_urls = ["http://restaurangedison.se/lunch"]
+    restaurant = "Ideon Edison"
 
     def parse(self, response):
-        weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
+        weekdays = ["monday", "tuesday", "wednesday", "thursday", "friday"]
         for day in weekdays:
             yield {
-                'weekday': day,
-                'courses': response.xpath('//div[@id=$val]/table//td[@class="course_description"]/p[1]/text()', val=day).getall()
+                "weekday": day,
+                "courses": response.xpath(
+                    '//div[contains(@class, $val)]//descendant::div[@class="lunchmeny_container"]/div',
+                    val=day,
+                )
+                .xpath("normalize-space(.)")
+                .extract(),
             }
